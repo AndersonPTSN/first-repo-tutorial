0cd33b6bddf574a39ee8b719e460368582d45623
Anderson <andersonptsn@yahoo.com.br>
2019-02-21 09:46:50 -0300

    Incrementando o arquivo hello_word.py

--- src/hello_word.py
+++ src/hello_word.py
@@ -1,2 +1,4 @@
 print("Olá Mundo!")
 print("Este sou eu seguindo o tutorial e vendo como o GIT funciona!")
+print("Se quiser conferir é só acessar:")
+print("https://github.com/AndersonPTSN/first-repo-tutorial")
